67503be4e9c77304c8b58196d447db36bd8d1961
SnaKl <62023405+SnaKl@users.noreply.github.com>
2021-11-25 17:13:27 +0100

    Jest config

---
+++ API/test/setupTests.js
@@ -0,0 +1,7 @@
+// import db from '../src/db';
+
+import * as matchers from 'jest-extended';
+expect.extend(matchers);
+
+// beforeEach(async () => await db.connect());
+// afterEach(() => db.disconnect());
