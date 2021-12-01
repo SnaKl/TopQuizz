daa1cea81d0038f2625cd844bb0af3b3ec35f444
SnaKl <62023405+SnaKl@users.noreply.github.com>
2021-12-01 16:19:40 +0100

    Work emily

--- API/src/question/question.validators.js
+++ API/src/question/question.validators.js
@@ -1,12 +1,23 @@
 import { body, check } from 'express-validator';
 
-export function updateQuestion() {
+export const QuestionValidator = {
+	// username must be an email
+	bodyDescriptionValidator: body('description').withMessage(
+		'Question description',
+	),
+	// password must be at least 5 chars long
+	bodyAnswerList: body(anwserList)
+		.isArray({ min: 4, max: 4 })
+		.withMessage('4 possible anwsers are required'),
+};
+
+export function createOrUpdateQuestion() {
 	return [
-		// username must be an email
-		body('description').withMessage('Question description'),
-		// password must be at least 5 chars long
-		body(anwserList)
-			.isArray({ min: 4, max: 4 })
-			.withMessage('4 possible anwsers are required'),
+		QuestionValidator.bodyDescriptionValidator,
+		QuestionValidator.bodyAnswerList,
 	];
+}
+
+export function updateQuestion() {
+	return;
 }
