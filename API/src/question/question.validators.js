ed2969823b49c6d8141f5d2cb3b565033b1907fb
e-renard <72547692+e-renard@users.noreply.github.com>
2021-12-01 18:39:18 +0100

    Theme and Question validators

--- API/src/question/question.validators.js
+++ API/src/question/question.validators.js
@@ -1,23 +1,48 @@
-import { body, check } from 'express-validator';
+import { body, param } from 'express-validator';
 
 export const QuestionValidator = {
 	// username must be an email
-	bodyDescriptionValidator: body('description').withMessage(
-		'Question description',
-	),
-	// password must be at least 5 chars long
-	bodyAnswerList: body(anwserList)
+	bodyDescriptionValidator: body('description')
+	.withMessage('Question description')
+	.notEmpty(),	
+
+	bodyAnswerListValidator: body('anwserList')
 		.isArray({ min: 4, max: 4 })
-		.withMessage('4 possible anwsers are required'),
+		.withMessage('4 possible anwsers are required')
+		.notEmpty(),
+	
+	bodyThemeValidator: body('_themeID')
+	.notEmpty()
+	.withMessage('Theme needs to be supplied to record a question'),
+
+	paramTheme : param('_themeID')
+	.notEmpty(),
+
+	optionalBodyDescription : body('description')
+	.notEmpty()
+	.optional(),
+
+	optionalBodyAnswerList : body('_themeID')
+	.notEmpty()
+	.optional(),
 };
 
 export function createOrUpdateQuestion() {
 	return [
 		QuestionValidator.bodyDescriptionValidator,
-		QuestionValidator.bodyAnswerList,
+		QuestionValidator.bodyAnswerListValidator,
+		QuestionValidator.bodyThemeValidator
 	];
 }
 
-export function updateQuestion() {
-	return;
+export function getQuestionByTheme(){
+	return [QuestionValidator.paramTheme];
+}
+
+export function deleteQuestionByTheme(){
+	return[
+		QuestionValidator.paramTheme,
+		QuestionValidator.optionalBodyDescription,
+		QuestionValidator.optionalBodyAnswerList
+	]
 }
