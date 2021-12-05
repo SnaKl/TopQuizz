f5d731e99b5f2c474123ad5c727e3dec7b5bde80
SnaKl <62023405+SnaKl@users.noreply.github.com>
2021-12-05 01:00:57 +0100

    Revert "User avatar"

--- API/src/auth/auth.validator.js
+++
@@ -1,7 +0,0 @@
-import { UserValidator } from '../user/user.validator';
-
-export function loginUser() {
-	return [UserValidator.bodyNickname, UserValidator.bodyPassword];
-}
-
-// export function connectUser() {}
--- API/test/setupTests.js
+++
@@ -1,4 +0,0 @@
-import db from '../src/config/db';
-
-beforeEach(async () => await db.connect());
-afterEach(() => db.disconnect());
