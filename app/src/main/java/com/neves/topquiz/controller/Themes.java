b4d025050825ce80af4cbbd18d41f3eb9b70bd57
richard-ioi <59491347+richard-ioi@users.noreply.github.com>
2021-12-05 19:49:42 +0100

    Bouton theme1 cliquable pour la démonstration
    On lance GameActivity lorsqu'on clique sur le premier thème. Utile pour la démonstration d'une shapeableImageView cliquable. Cependant l'application crash car on passe aucune instance d'User ce qui est tout à fait normal.

--- app/src/main/java/com/neves/topquiz/controller/Themes.java
+++ app/src/main/java/com/neves/topquiz/controller/Themes.java
@@ -1,15 +1,30 @@
 package com.neves.topquiz.controller;
 
+import com.google.android.material.imageview.ShapeableImageView;
 import com.neves.topquiz.R;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Themes extends AppCompatActivity {
+    private ShapeableImageView mTheme1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_themes);
+        mTheme1 = findViewById(R.id.themes_theme1_btn);
+
+        mTheme1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ChooseTheme = new Intent(Themes.this, GameActivity.class);
+                startActivity(ChooseTheme);
+            }
+        });
     }
+
 }
