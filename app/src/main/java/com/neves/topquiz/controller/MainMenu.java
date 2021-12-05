6666b30f6759446633d0338c95e290ff10e1888b
richard-ioi <59491347+richard-ioi@users.noreply.github.com>
2021-12-05 19:30:52 +0100

    Correction boutons main menu

--- app/src/main/java/com/neves/topquiz/controller/MainMenu.java
+++ app/src/main/java/com/neves/topquiz/controller/MainMenu.java
@@ -11,34 +11,35 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainMenu extends AppCompatActivity {
-    private ImageButton chooseTheme;
-    private ImageButton mMyAccount;
-    private ImageButton mLeaderboard;
-    private ImageButton mCreateQuiz;
-    private ImageButton mValidateQuiz;
+    private LinearLayout mChooseTheme;
+    private LinearLayout mMyAccount;
+    private LinearLayout mLeaderboard;
+    private LinearLayout mCreateQuiz;
+    private LinearLayout mValidateQuiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        chooseTheme= (ImageButton) findViewById(R.id.main_menu_chooseTheme_btn);
-        mMyAccount = (ImageButton) findViewById(R.id.main_menu_account_btn);
-        mLeaderboard = (ImageButton) findViewById(R.id.main_menu_leaderboard_btn);
-        mCreateQuiz = (ImageButton) findViewById(R.id.main_menu_createQuiz_btn);
-        mValidateQuiz = (ImageButton) findViewById(R.id.main_menu_validateQuiz_btn);
+        mChooseTheme= findViewById(R.id.main_menu_chooseTheme_btn);
+        mMyAccount = findViewById(R.id.main_menu_account_btn);
+        mLeaderboard = findViewById(R.id.main_menu_leaderboard_btn);
+        mCreateQuiz = findViewById(R.id.main_menu_createQuiz_btn);
+        mValidateQuiz = findViewById(R.id.main_menu_validateQuiz_btn);
 
-        chooseTheme.setOnClickListener(new View.OnClickListener() {
+        mChooseTheme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Intent ChooseThemeIntent = ;
-                startActivity(new Intent(MainMenu.this, Themes.class));
+                Intent ChooseTheme = new Intent(MainMenu.this, Themes.class);
+                startActivity(ChooseTheme);
             }
         });
 
@@ -55,22 +56,6 @@ public class MainMenu extends AppCompatActivity {
             public void onClick(View v) {
                 Intent Leaderboard = new Intent(MainMenu.this, LeaderboardGeneral.class);
                 startActivity(Leaderboard);
-            }
-        });
-
-        mCreateQuiz.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent CreateQuiz = new Intent(MainMenu.this, Profile.class);
-                startActivity(CreateQuiz);
-            }
-        });
-
-        mValidateQuiz.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent ValidateQuiz = new Intent(MainMenu.this, Profile.class);
-                startActivity(ValidateQuiz);
             }
         });
     }
