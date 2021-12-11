b77c01d0dd4b4805cd84afe95f69569996b9973e
SnaKl <62023405+SnaKl@users.noreply.github.com>
2021-12-12 00:01:47 +0100

    Global variable

---
+++ App/app/src/main/java/com/neves/topquiz/GlobalVariable.java
@@ -0,0 +1,8 @@
+package com.neves.topquiz;
+
+public final class GlobalVariable {
+    public static final String API_URL = "192.168.0.14";
+    public static final String API_PORT = "3000";
+
+    private GlobalVariable(){ }
+}
