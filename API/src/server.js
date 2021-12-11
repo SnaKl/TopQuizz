e4aa5244ce91cf630ebdd46861878dd5be69f4c3
SnaKl <62023405+SnaKl@users.noreply.github.com>
2021-12-11 12:51:55 +0100

    API on network

--- API/src/server.js
+++ API/src/server.js
@@ -1,12 +1,13 @@
 import db from './config/db';
 import app from './app';
+var http = require('http');
 
 const serverUrl = process.env.SERVER_ADDRESS + ':' + process.env.SERVER_PORT;
 
 db.connect();
 db.mongoose.connection.once('open', () => {
 	console.log('Database connected');
-	app.listen(process.env.SERVER_PORT, () => {
+	app.listen(3000, '0.0.0.0', () => {
 		console.log(`API listening on ${serverUrl}`);
 	});
 });
