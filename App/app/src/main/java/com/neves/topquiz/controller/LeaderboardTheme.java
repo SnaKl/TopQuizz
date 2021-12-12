ded31aaf7ffcc2e2e2e239226c5bcf060ef42b9b
abrkgts <abarnakgts@gmail.com>
2021-12-12 12:50:50 +0100

    Theme spinner for the leaderboard

--- App/app/src/main/java/com/neves/topquiz/controller/LeaderboardTheme.java
+++ App/app/src/main/java/com/neves/topquiz/controller/LeaderboardTheme.java
@@ -3,51 +3,68 @@ package com.neves.topquiz.controller;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.imageview.ShapeableImageView;
 import com.neves.topquiz.R;
+import com.neves.topquiz.model.Theme;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LeaderboardTheme extends AppCompatActivity {
 
     private Button mGeneralBtn;
     private Button mThemeBtn;
+    private Spinner mThemeLeaderboard;
     private ShapeableImageView mSilver;
     private ShapeableImageView mGold;
     private ShapeableImageView mBronze;
     private ShapeableImageView mProfPic1;
-    private ShapeableImageView mProfPic2;
-    private ShapeableImageView mProfPic3;
-    private ShapeableImageView mProfPic4;
-    private ShapeableImageView mProfPic5;
     private TextView mPlayer1;
-    private TextView mPlayer2;
-    private TextView mPlayer3;
-    private TextView mPlayer4;
-    private TextView mPlayer5;
+    private Theme mTheme;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard_theme);
-
+        mThemeLeaderboard = findViewById(R.id.leaderboard_themes_spnr);
         mGeneralBtn = (Button) findViewById(R.id.leaderboard_general_btn);
         mThemeBtn = (Button) findViewById(R.id.leaderboard_themes_btn);
         mSilver = (ShapeableImageView) findViewById(R.id.leaderboard_secondWinner_profilePic_siv);
         mGold = (ShapeableImageView) findViewById(R.id.leaderboard_firstWinner_profilePic_siv);
         mBronze = (ShapeableImageView) findViewById(R.id.leaderboard_thirdWinner_profilePic_siv);
         mProfPic1 = (ShapeableImageView) findViewById(R.id.leaderboard_profilePicture1_siv);
-        mProfPic2 = (ShapeableImageView) findViewById(R.id.leaderboard_profilePicture2_siv);
-        mProfPic3 = (ShapeableImageView) findViewById(R.id.leaderboard_profilePicture3_siv);
-        mProfPic4 = (ShapeableImageView) findViewById(R.id.leaderboard_profilePicture4_siv);
-        mProfPic5 = (ShapeableImageView) findViewById(R.id.leaderboard_profilePicture5_siv);
         mPlayer1 = (TextView) findViewById(R.id.leaderboard_name1_tv);
-        mPlayer2 = (TextView) findViewById(R.id.leaderboard_name2_tv);
-        mPlayer3 = (TextView) findViewById(R.id.leaderboard_name3_tv);
-        mPlayer4 = (TextView) findViewById(R.id.leaderboard_name4_tv);
-        mPlayer5 = (TextView) findViewById(R.id.leaderboard_name5_tv);
+
+        List<Theme> spinnerArray =  new ArrayList<>();
+        spinnerArray.add(new Theme("", "THE OFFICE", "TropLol", 5));
+        spinnerArray.add(new Theme("", "FRIENDS", "TropLol", 5));
+        spinnerArray.add(new Theme("", "COMMUNITY", "TropLol", 5));
+
+        CustomAdapter adapter = new CustomAdapter(LeaderboardTheme.this,
+                R.layout.spinner_item_layout_resource,
+                R.id.textView_item_name,
+                spinnerArray);
+        mThemeLeaderboard.setAdapter(adapter);
+
+        mThemeLeaderboard.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()  {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mTheme = (Theme) spinnerArray.get(position);
+                System.out.println(mTheme.getTitle());
+                System.out.println("mTheme.getTitle()!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         mGeneralBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,14 +75,6 @@ public class LeaderboardTheme extends AppCompatActivity {
             }
         });
 
-//        mThemeBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //Intent ChooseThemeIntent = ;
-//                startActivity(new Intent(LeaderboardGeneral.this, LeaderboardTheme.class));
-//                finish();
-//            }
-//        });
     }
 }
 
