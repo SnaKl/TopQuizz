bc7528de98d1652c11788710ae0e331f17143336
richard-ioi <59491347+richard-ioi@users.noreply.github.com>
2021-12-12 12:06:57 +0100

    Profile+EditProfile OK

--- App/app/src/main/java/com/neves/topquiz/controller/Profile.java
+++ App/app/src/main/java/com/neves/topquiz/controller/Profile.java
@@ -10,19 +10,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.imageview.ShapeableImageView;
 import com.neves.topquiz.R;
+import com.neves.topquiz.model.User;
 
 public class Profile extends AppCompatActivity {
+    private User mUser;
     private ShapeableImageView mProfPic;
     private TextView mPointsNumber;
     private TextView mUsername;
     private TextView mMail;
     private TextView mPassword;
     private ImageButton mEditBtn;
+    private static final String USER = "USER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(USER)) {
+            mUser = intent.getParcelableExtra(USER);
+        }
 
         mProfPic  = (ShapeableImageView) findViewById(R.id.profile_picture_siv);
         mPointsNumber = (TextView) findViewById(R.id.profile_nbPoints_tv);
@@ -31,16 +39,18 @@ public class Profile extends AppCompatActivity {
         mPassword = (TextView) findViewById(R.id.profile_password_input);
         mEditBtn = (ImageButton) findViewById(R.id.profile_edit_btn);
 
-        //UserDB userDB = new UserDB(Parametres.this.getContext());
-        //mUsername.setText(userDB.getAll().getString(0));
-        //mMail.setText(userDB.getAll().getString(0));
-        //mPassword.setText(userDB.getAll().getString(0));
+        mUsername.setText(mUser.getNickname());
+        mMail.setText(mUser.getEmail());
+        mPassword.setText("●●●●●●●●●●●");
+        new DownLoadImageTask(mProfPic).execute(mUser.getAvatar());
+
 
         mEditBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent ChooseThemeIntent = new Intent(Profile.this, EditProfile.class);
-                startActivity(ChooseThemeIntent);
+                Intent EditAccountIntent = new Intent(Profile.this, EditProfile.class);
+                EditAccountIntent.putExtra(USER, mUser);
+                startActivity(EditAccountIntent);
                 finish();
             }
         });
