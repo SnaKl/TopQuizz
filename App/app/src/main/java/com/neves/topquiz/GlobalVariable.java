58b00aaeb2055b2104dbd3776bf30e1c0d6bdd1c
SnaKl <62023405+SnaKl@users.noreply.github.com>
2021-12-12 02:57:58 +0100

    Revert "Display theme from api"

--- App/app/src/main/java/com/neves/topquiz/GlobalVariable.java
+++
@@ -1,9 +0,0 @@
-package com.neves.topquiz;
-
-public final class GlobalVariable {
-    public static final String API_HOST = "192.168.137.1";
-    public static final String API_PORT = "3000";
-    public static final String API_URL = "http://"+API_HOST+":"+API_PORT;
-
-    private GlobalVariable(){ }
-}
